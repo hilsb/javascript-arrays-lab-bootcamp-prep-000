e79c961cfdf60bac0dc1c4260b9fd7b309d27159
Hilary Sbei <hilsbei@gmail.com>
2017-12-14 21:29:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function destructivelyRemoveFirstKitten(name) {
 
 function appendKitten(name) {
   kittens = ["Milo", "Otis", "Garfield"];
-  [...kittens, name];
+  [...kittens, "Broom"];
   return kittens;
 }
 
