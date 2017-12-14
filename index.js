5ba8a6aa0b1b955566b2c65c62091b4683b8d3c4
Hilary Sbei <hilsbei@gmail.com>
2017-12-14 19:46:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ function destructivelyAppendKitten(name) {
     kittens.push(name);
     return kittens;
 }
+
+function destructivelyPrependKitten(name) {
+  kittens = ["Milo", "Otis", "Garfield"];
+  kittens.unshift(name);
+  return kittens;
+}
